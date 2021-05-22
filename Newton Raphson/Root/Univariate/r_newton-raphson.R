50cdb1b2e49750af945b1eeacff763a0b7d0592a
Anders Rye Jacobsen <andersrye.jacobsen@gmail.com>
2021-05-22 17:38:37 +0200

    modified NR to return list in R

--- Newton Raphson/Root/Univariate/r_newton-raphson.R
+++ Newton Raphson/Root/Univariate/r_newton-raphson.R
@@ -16,16 +16,30 @@ newton_raphson <- function(func,
 
     while ((iter < maxiter) && (abs(func(x_0, ...)) > tol)) {
         x_0 <- x_0 - (func(x_0, ...) / func_p(x_0, ...))
-
         iter <- iter + 1
+
+        # comment out to ignore outputs
         cat(iter, ":", "x =", x_0, "\n")
     }
 
     if (abs(func(x_0, ...)) < tol) {
-        cat("Root successfully found\n")
-        return(x_0)
+        return(
+            list(
+                root = x_0,
+                iterations = iter,
+                value_at_root = func(x_0),
+                convergence = "Achieved"
+            )
+        )
     } else {
-        stop("Failed to converge")
+        return(
+            list(
+                root = NULL,
+                iterations = iter,
+                value_at_root = NULL,
+                convergence = "Not achieved"
+            )
+        )
     }
 }
 
