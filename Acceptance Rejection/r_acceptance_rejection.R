80531bd02f77f168590836f7b23046a3678fe760
Anders Rye Jacobsen <andersrye.jacobsen@gmail.com>
2021-05-23 14:12:35 +0200

    formatting

--- Acceptance Rejection/r_acceptance_rejection.R
+++ Acceptance Rejection/r_acceptance_rejection.R
@@ -1,24 +1,17 @@
 ### Auxiliary functions ###
 
 gamma.pdf <- function(x, k, theta) {
-  
-  return( theta^k * x^(k-1) * exp(-theta*x) )
-  
+    return(theta^k * x^(k - 1) * exp(-theta * x))
 }
 
 exponential.pdf <- function(x, lambda) {
-  
-  return( lambda * exp(-lambda*x))
-  
+    return(lambda * exp(-lambda * x))
 }
 
 Exponential.Simulate <- function(lambda, size = 1) {
-  
-  V <- runif(size)
-  return(-1/lambda * log(V))
-  
+    V <- runif(size)
+    return(-1 / lambda * log(V))
 }
-
 
 
 ### Simulate random variable having Gamma distribution ###
@@ -29,38 +22,34 @@ Exponential.Simulate <- function(lambda, size = 1) {
 #
 ###
 
-# Note: Man skal sætte værdier for theta og k, hvilket jeg selv gør for nedenstående, hvis man skal gøre linjerne enkeltvis inde i funktionen. 
+# Note: Man skal sætte værdier for theta og k, hvilket jeg selv gør for nedenstående, hvis man skal gøre linjerne enkeltvis inde i funktionen.
 # theta <- 0.1
-# k <- 1.1 
+# k <- 1.1
 
 
 Gamma.Simulate <- function(k, theta, size = 1) {
-  
-  lambda <- theta/k
-  c <- k^k * exp(-k+1) / gamma(k)
-  
-  U <- rep(NA, size)
-  Y <- rep(NA, size)
-  X <- rep(NA, size)
-  Unaccepted <- rep(TRUE, size)
-  
-  while (any(Unaccepted)) {
-    
-    UnacceptedCount <- sum(Unaccepted)
-    
-    U <- runif(UnacceptedCount)
-    Y <- Exponential.Simulate(lambda, UnacceptedCount)
-    
-    Accepted_ThisTime <- Unaccepted[Unaccepted] & 
-      ( U <= ( gamma.pdf(Y, k, theta) / exponential.pdf(Y, lambda)/c ) ) # burde det ikke være gange c?
-                        
-    X[Unaccepted][Accepted_ThisTime] <- Y[Accepted_ThisTime]
-    Unaccepted[Unaccepted] <- !Accepted_ThisTime
-    
-  }
-  
-  return(X)
-  
+    lambda <- theta / k
+    c <- k^k * exp(-k + 1) / gamma(k)
+
+    U <- rep(NA, size)
+    Y <- rep(NA, size)
+    X <- rep(NA, size)
+    Unaccepted <- rep(TRUE, size)
+
+    while (any(Unaccepted)) {
+        UnacceptedCount <- sum(Unaccepted)
+
+        U <- runif(UnacceptedCount)
+        Y <- Exponential.Simulate(lambda, UnacceptedCount)
+
+        Accepted_ThisTime <- Unaccepted[Unaccepted] &
+            (U <= (gamma.pdf(Y, k, theta) / exponential.pdf(Y, lambda) / c)) # burde det ikke være gange c?
+
+        X[Unaccepted][Accepted_ThisTime] <- Y[Accepted_ThisTime]
+        Unaccepted[Unaccepted] <- !Accepted_ThisTime
+    }
+
+    return(X)
 }
 
 Gamma.Simulate(k, theta, size = 1)
@@ -69,18 +58,24 @@ Gamma.Simulate(k, theta, size = 1)
 
 set.seed(10086)
 
-system.time({X <- Gamma.Simulate(2, 1, 10^6)})
+system.time({
+    X <- Gamma.Simulate(2, 1, 10^6)
+})
 
 # histogram
 
-hist(X, breaks = 30, freq = FALSE, 
-     main = "Theoretical and simulated Gamma(2,1) density", 
-     col = "cornflowerblue", 
-     xlim = c(0, 14), ylim = c(0,0.35), 
-     xlab = "x",
-     cex.main = 0.8)
+hist(X,
+    breaks = 30, freq = FALSE,
+    main = "Theoretical and simulated Gamma(2,1) density",
+    col = "cornflowerblue",
+    xlim = c(0, 14), ylim = c(0, 0.35),
+    xlab = "x",
+    cex.main = 0.8
+)
 
-xticks = seq(0, max(X), 0.1)
+xticks <- seq(0, max(X), 0.1)
 lines(xticks, dgamma(xticks, 2, 1), col = "red")
-legend("topright", legend = c("Simulated", "Theoretical"), lty = c(1, 1),
-       lwd = c(5, 1), col = c("cornflowerblue", "red"))
+legend("topright",
+    legend = c("Simulated", "Theoretical"), lty = c(1, 1),
+    lwd = c(5, 1), col = c("cornflowerblue", "red")
+)
