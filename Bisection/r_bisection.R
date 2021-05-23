96789412b1d36aa46d913176f2ab10e9181ecc4a
Anders Rye Jacobsen <andersrye.jacobsen@gmail.com>
2021-05-23 12:23:04 +0200

    refactor bisection

--- Bisection/r_bisection.R
+++ Bisection/r_bisection.R
@@ -19,7 +19,14 @@ bisection <- function(func, x_l, x_r, tol = 1e-5, ...) {
         f_x_m <- func(x_m, ...)
 
         if (f_x_m == 0) {
-            return(x_m)
+            return(
+                list(
+                    iterations = iter,
+                    convergence = "Achieved",
+                    root = x_m,
+                    func.root = f_x_m
+                )
+            )
         } else if (func(x_l, ...) * func(x_m, ...) < 0) {
             x_r <- x_m
         } else {
@@ -28,9 +35,26 @@ bisection <- function(func, x_l, x_r, tol = 1e-5, ...) {
         iter <- iter + 1
         cat(iter, ":", x_l, "< root <", x_r, "\n")
     }
-
-    return((x_r + x_l) / 2)
+    return(
+        list(
+            iterations = iter,
+            convergence = "Achieved",
+            root = (x_r + x_l) / 2,
+            func.root = func(((x_r + x_l) / 2), ...)
+        )
+    )
 }
 
 # calculate roots
 bisection(objective, -10, 10, m = 15, l = 0.1)
+
+# plot function
+plot(
+    seq(-10, 10, 1e-3),
+    objective(seq(-10, 10, 1e-3), m = 15, l = 0.1),
+    type = "l"
+)
+abline(h = 0, col = "red")
+
+# verify results
+uniroot(objective, interval = c(-10, 10), l = 0.1, m = 15)
