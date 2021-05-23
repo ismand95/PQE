86e91cadceed0eac216806ffe025c96837eba4ea
jakobahlers <84626187+jakobahlers@users.noreply.github.com>
2021-05-23 13:55:03 +0200

    Update r_inversion.R

--- Inversion Method/r_inversion.R
+++ Inversion Method/r_inversion.R
@@ -1,5 +1,17 @@
 set.seed(1337)
 
+# Inversion method
+# Template for inverse
+inversion_method <- function(iT, #write variables) {
+   # iT is the number of draws we wish from the uniform distribution
+
+    vU <- runif(iT) # drawing from uniform distribution
+    # dOut <- 'write code here' # Here we provide the inverse/quantile function
+
+    return(dOut)
+}
+
+
 # Inversion method with Gumbel distribution 
 gumbel.simulate <- function(n, beta = 2, mu = 0.5) {
     data_sample <- runif(n, min = 0, max = 1)
